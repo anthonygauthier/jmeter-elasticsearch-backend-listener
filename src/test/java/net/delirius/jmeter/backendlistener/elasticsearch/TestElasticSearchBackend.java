f4054c17b3027ba58c7222ce8f5ff0b18ec5e47f
pmouawad <p.mouawad@ubik-ingenierie.com>
2018-01-17 17:22:29 +0100

    This fixes #2 #4 #5 #6 Add timeout when sending data to ES getElasticData : ResponseTime is not correct Add metric sentBytes YYYY format is wrong in code Remove printStackTrace Add constants

--- src/test/java/net/delirius/jmeter/backendlistener/elasticsearch/TestElasticSearchBackend.java
+++ src/test/java/net/delirius/jmeter/backendlistener/elasticsearch/TestElasticSearchBackend.java
@@ -1,19 +1,14 @@
 package net.delirius.jmeter.backendlistener.elasticsearch;
 
-import org.apache.jmeter.samplers.SampleResult;
-import org.apache.jmeter.threads.JMeterContext;
-import org.apache.jmeter.threads.JMeterContextService;
-import org.apache.jmeter.visualizers.backend.BackendListenerContext;
-import org.junit.Before;
-import org.junit.Test;
+import static org.junit.Assert.assertNotNull;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
 
-import static org.junit.Assert.assertNotNull;
+import org.apache.jmeter.threads.JMeterContext;
+import org.apache.jmeter.threads.JMeterContextService;
+import org.junit.Before;
+import org.junit.Test;
 
 public class TestElasticSearchBackend {
     private DummyElasticSearchBackend instance = null;
@@ -33,7 +28,7 @@ public class TestElasticSearchBackend {
 
     @Test
     public void testGetElapsedTime() throws Exception {
-        SimpleDateFormat sdf = new SimpleDateFormat("YYYY-mm-dd HH:mm:ss");
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         Date testDate = this.instance.getElapsedDate();
         assertNotNull("testDate = " + sdf.format(testDate), sdf.format(testDate));
     }
