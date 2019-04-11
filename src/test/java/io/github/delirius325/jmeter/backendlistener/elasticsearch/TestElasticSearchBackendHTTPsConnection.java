cf07ed841a50cd82cb303ff9f503865750d7be82
ChristianManuelWeber <christian-manuel.weber@deutschebahn.com>
2019-04-11 20:07:02 +0200

    SSL Test added

--- src/test/java/io/github/delirius325/jmeter/backendlistener/elasticsearch/TestElasticSearchBackendHTTPsConnection.java
+++ src/test/java/io/github/delirius325/jmeter/backendlistener/elasticsearch/TestElasticSearchBackendHTTPsConnection.java
@@ -3,6 +3,7 @@ package io.github.delirius325.jmeter.backendlistener.elasticsearch;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.apache.commons.io.FilenameUtils;
 import org.apache.http.HttpHost;
 import org.apache.logging.log4j.core.config.Configurator;
 import org.elasticsearch.client.Node;
@@ -19,6 +20,21 @@ public class TestElasticSearchBackendHTTPsConnection {
     public void setUp() throws Exception {
         Configurator.initialize(null, "config/log4j2.xml");
         SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
+
+        String SSL_TRUSTSTORE_PATH = "certs/truststore.jks";
+        String SSL_TRUSTSTORE_PW = "changeit";
+        String SSL_KEYSTORE_PATH = "certs/logstash-keystore.jks";
+        String SSL_KEYSTORE_PW = "changeit";
+
+        System.setProperty("javax.net.ssl.keyStore", SSL_KEYSTORE_PATH);
+        System.setProperty("javax.net.ssl.keyStorePassword", SSL_KEYSTORE_PW);
+        System.setProperty("javax.net.ssl.keyStoreType",
+                FilenameUtils.getExtension(SSL_KEYSTORE_PATH).toLowerCase().equals("jks") ? "JKS" : "PKCS12");
+
+        System.setProperty("javax.net.ssl.trustStore", SSL_TRUSTSTORE_PATH);
+        System.setProperty("javax.net.ssl.trustStorePassword", SSL_TRUSTSTORE_PW);
+        System.setProperty("javax.net.ssl.trustStoreType",
+                FilenameUtils.getExtension(SSL_TRUSTSTORE_PATH).toLowerCase().equals("jks") ? "JKS" : "PKCS12");
 
         client = RestClient.builder(new HttpHost("localhost", Integer.parseInt("9200"), "https"))
                 .setRequestConfigCallback(requestConfigBuilder -> requestConfigBuilder.setConnectTimeout(5000)
